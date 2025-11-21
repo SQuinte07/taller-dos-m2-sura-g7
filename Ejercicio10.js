4610fada894d7d76efc9b458282e9de636ac58ca
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 12:01:48 -0500

    Ej10

---
+++ Ejercicio10.js
@@ -0,0 +1,8 @@
+let carrito = ["Manzana"];
+
+console.log("Carrito original:", carrito);
+
+let nuevoCarrito = carrito.concat("Pera");
+
+console.log("Nuevo carrito:", nuevoCarrito);
+console.log("Carrito original sigue igual:", carrito);
