0ffcbe07301e32dae788a467ea1d528094215835
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:59:28 -0500

    Ej8

---
+++ Ejercicio8.js
@@ -0,0 +1,8 @@
+let competidores = ["Oro", "Plata", "Bronce", "Mención 1", "Mención 2"];
+
+console.log("Lista de competidores:", competidores);
+
+let podio = competidores.slice(0, 3);
+
+console.log("Podio (primeros 3):", podio);
+console.log("Lista original sigue intacta:", competidores);
