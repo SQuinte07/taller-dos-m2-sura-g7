8850381fc4835ea16f86125fa9ec0ed69754c4ee
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 11:40:00 -0500

    Ej1

---
+++ Ejercicio1.js
@@ -0,0 +1,8 @@
+let puntos = [10, 20, 30, 40];
+
+console.log("Arreglo original:", puntos);
+
+let puntosDoble = puntos.map(num => num * 2);
+
+console.log("Nuevo arreglo con valores duplicados:", puntosDoble);
+console.log("Arreglo original sigue igual:", puntos);
