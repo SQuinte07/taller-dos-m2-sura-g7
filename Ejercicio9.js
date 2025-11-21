c520104b8bf2fac1a892f7ddb888ce7c6268fa7f
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 12:00:37 -0500

    Ej9

---
+++ Ejercicio9.js
@@ -0,0 +1,10 @@
+let equipoDev = ["Ana", "Juan"];
+let equipoQA = ["Carlos", "Maria"];
+
+console.log("Equipo Dev:", equipoDev);
+console.log("Equipo QA:", equipoQA);
+
+let equipoCompleto = equipoDev.concat(equipoQA);
+
+console.log("Equipo completo:", equipoCompleto);
+console.log("Equipos originales siguen intactos:", equipoDev, equipoQA);
