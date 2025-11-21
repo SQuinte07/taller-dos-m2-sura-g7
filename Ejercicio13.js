4738c36e38e49513f01dcb7cea92f1c20e2ef33a
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 12:05:37 -0500

    Ej13

---
+++ Ejercicio13.js
@@ -0,0 +1,8 @@
+let invitados = ["Zapata", "Alvarez", "Gomez", "Bernal"];
+
+console.log("Lista original de invitados:", invitados);
+
+let invitadosOrdenados = [...invitados].sort();
+
+console.log("Lista de invitados ordenada:", invitadosOrdenados);
+console.log("Lista original sigue igual:", invitados);
