a521333b7ffe104c4d942aae86e463ad8f5abe7a
SQuinte07 <sebast.quinte13@gmail.com>
2025-11-21 12:06:38 -0500

    Ej14

---
+++ Ejercicio14.js
@@ -0,0 +1,8 @@
+let receta = ["Paso 1", "Paso 2", "Paso 3"];
+
+console.log("Receta original:", receta);
+
+let recetaInversa = [...receta].reverse();
+
+console.log("Receta invertida:", recetaInversa);
+console.log("Receta original sigue igual:", receta);
